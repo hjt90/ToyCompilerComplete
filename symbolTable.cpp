280c57bc3e3111daa2c7ee5c1ffe3c0976041ea6
hjt90 <642712787@qq.com>
2021-12-12 22:51:21 +0800

    debug

--- symbolTable.cpp
+++ symbolTable.cpp
@@ -1,63 +1,64 @@
 #include "symbolTable.h"
+using namespace std;
 
 int proc_symbolTable::nextname = 0;
 int proc_symbolTable::nexttmpname = 0;
 
-const symbolTableItem &proc_symbolTable::find_variable(std::string name)
+const symbolTableItem& proc_symbolTable::find_variable(std::string name)
 {
-    if (itemTable.find(name) != itemTable.end())
-    {
-        return itemTable[name];
-    }
-    symbolTableItem tmp;
-    tmp.type = symbolType::None;
-    return tmp;
+	if (itemTable.find(name) != itemTable.end())
+	{
+		return itemTable[name];
+	}
+	symbolTableItem tmp;
+	tmp.type = symbolType::None;
+	return tmp;
 }
 
-const symbolTableFunction &proc_symbolTable::find_function(std::string name)
+const symbolTableFunction& proc_symbolTable::find_function(std::string name)
 {
-    if (functionTable.find(name) != functionTable.end())
-    {
-        return functionTable[name];
-    }
-    symbolTableFunction tmp;
-    tmp.return_type = symbolType::None;
-    return tmp;
+	if (functionTable.find(name) != functionTable.end())
+	{
+		return functionTable[name];
+	}
+	symbolTableFunction tmp;
+	tmp.return_type = symbolType::None;
+	return tmp;
 }
 
-void proc_symbolTable::make_function(const std::vector<symbolTableItem> &parms, std::string name, symbolType return_type, int enter_quad)
+void proc_symbolTable::make_function(const std::vector<symbolTableItem>& parms, std::string name, symbolType return_type, int enter_quad)
 {
-    symbolTableFunction func;
-    func.name = name;
-    func.return_type = return_type;
-    func.parm = parms;
-    func.enter_quad = enter_quad;
-    func.addr = new proc_symbolTable();
-    func.addr->returnAddr = this;
+	symbolTableFunction func;
+	func.name = name;
+	func.return_type = return_type;
+	func.parm = parms;
+	func.enter_quad = enter_quad;
+	func.addr = new proc_symbolTable();
+	func.addr->returnAddr = this;
 
-    this->functionTable.insert({name, func});
-    return;
+	this->functionTable.insert({ name, func });
+	return;
 }
 
-void proc_symbolTable::insert_variable(const symbolTableItem &item)
+void proc_symbolTable::insert_variable(const symbolTableItem& item)
 {
-    this->itemTable.insert({item.name, item});
+	this->itemTable.insert({ item.name, item });
 }
 
-proc_symbolTable *proc_symbolTable::into_function(std::string name)
+proc_symbolTable* proc_symbolTable::into_function(std::string name)
 {
-    return this->find_function(name).addr;
+	return this->find_function(name).addr;
 }
 
-proc_symbolTable *proc_symbolTable::return_function()
+proc_symbolTable* proc_symbolTable::return_function()
 {
-    return this->returnAddr;
+	return this->returnAddr;
 }
 
 string proc_symbolTable::newtemp()
 {
-    string temp="T";
-    temp += std::to_string(nexttmpname);
-    nexttmpname++;
-    return temp;
+	string temp = "T";
+	temp += std::to_string(nexttmpname);
+	nexttmpname++;
+	return temp;
 }
--- symbolTable.h
+++ symbolTable.h
@@ -44,5 +44,5 @@ public:
     void insert_variable(const symbolTableItem &);
     proc_symbolTable *into_function(std::string);
     proc_symbolTable *return_function();
-    string newtemp();
+    std::string newtemp();
 };
